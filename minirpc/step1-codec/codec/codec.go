601a6cd8a81cc02640359a10b89482d7bbca1827
jihonghe <1654508502@qq.com>
2022-07-06 23:02:10 +0800

    dev: implement the codec protocol exchange

---
+++ minirpc/step1-codec/codec/codec.go
@@ -0,0 +1,34 @@
+package codec
+
+import (
+	"io"
+)
+
+type Header struct {
+	ServiceMethod string // format "Service.Method"
+	Seq           uint64 // sequence number of a request
+	Error         string // if error occurs in server, then set the error
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(header *Header) (err error)
+	ReadBody(body interface{}) (err error)
+	Write(header *Header, content interface{}) (err error)
+}
+
+type NewCodecFunc func(closer io.ReadWriteCloser) (c Codec)
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json"
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
+}
